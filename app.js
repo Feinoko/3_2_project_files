b7601e6fac801015e4d875edcc73559f28787dbe
feinoko <feinoko@gmail.com>
2020-12-06 14:16:36 +0100

    1st commit

---
+++ app.js
@@ -0,0 +1,59 @@
+// let val;
+
+// val = document;
+// val = document.all;
+// val = document.all[2];
+// val = document.all.length;
+// val = document.head;
+// val = document.body;
+// val = document.doctype;
+// val = document.domain;
+// val = document.URL;
+// val = document.characterSet;
+// val = document.contentType;
+
+// val = document.forms;
+// val = document.forms[0];
+// val = document.forms[0].id;
+// val = document.forms[0].method;
+// val = document.forms[0].action;
+
+// val = document.links;
+// val = document.links[0];
+// val = document.links[0].id;
+// val = document.links[0].className;
+// val = document.links[0].classList[0];
+
+// val = document.images;
+
+// val = document.scripts;
+// val = document.scripts[2].getAttribute('src');
+
+// let scripts = document.scripts;
+
+// let scriptsArr = Array.from(scripts);
+
+// scriptsArr.forEach(function(script) {
+//   console.log(script.getAttribute('src'));
+// });
+
+// console.log(val);
+
+let val;
+val = document;
+val = document.all[4];
+val = document.head;
+val = document.forms[0].id;
+val = document.scripts[2].getAttribute('src');
+
+const scripts = document.scripts;
+val = [];
+let attr = [];
+
+for (let i = 0; i < scripts.length; i++) {
+  val.push(scripts[i]);
+  attr.push(val[i].getAttribute('src'));
+}
+
+console.log(val);
+console.log(attr);
